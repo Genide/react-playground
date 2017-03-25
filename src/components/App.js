c9ac4aac4f8882a2aca0a8783a59751233162be5
Daniel Huan Nguyen <danielhuannguyen@gmail.com>
2017-03-25 17:46:45 -0500

    Added dynamic textarea

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: "Hello World"
+    };
+  }
+
+  onChangeText(e) {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input
+          type="textarea"
+          id="textInput"
+          value={this.state.text}
+          onChange={this.onChangeText.bind(this)}/>
+        <br/>
+        <span id="displayArea">{this.state.text}</span>
       </div>
     );
   }
